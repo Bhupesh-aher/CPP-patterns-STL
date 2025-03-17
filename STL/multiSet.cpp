f7934fc8ea87139b6d08a67ec25facf392e09ef9
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-03-17 23:59:39 +0530

    MultiSet

---
+++ STL/multiSet.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    
+    multiset<int> ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(3);
+    ms.insert(3);
+    ms.insert(4);
+
+    // int count = ms.count(3);
+    // cout << "Count before deletion " << count << endl;
+
+    // ms.erase(3);
+    // int count2 = ms.count(3);
+    // cout << "Count after deletion " << count2 << endl;
+
+
+    // int cont = ms.count(1);
+    // cout << "Count before deletion " << cont << endl;
+
+    // ms.erase(ms.find(1));
+    // int cont1 = ms.count(1);
+    // cout << "Count after deletion " << cont1 << endl;
+
+
+
+
+}
